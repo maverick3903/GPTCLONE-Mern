76f7e23fff490f149a89220ff0b248bdcd1bd4dd
Yash Dubey <yashdubey20@gmail.com>
2024-01-08 22:36:52 +0530

    Secure Backend Complete

--- backend/src/routes/user-routes.ts
+++ backend/src/routes/user-routes.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
-import { getAlluser, userLogin, userSignup } from "../controllers/user-controllers.js";
+import { getAlluser, userLogin, userSignup, verifyUser } from "../controllers/user-controllers.js";
 import { loginValidator, signupValidator, validate } from "../utils/validtors.js";
+import { verifyToken } from "../utils/tokens-manager.js";
 
 const useRouter = Router();
 
 useRouter.get('/', getAlluser);
-
 useRouter.post('/signup',validate(signupValidator), userSignup);
-
 useRouter.post('/login',validate(loginValidator), userLogin);
+useRouter.get('/auth-status', verifyToken, verifyUser);
+
 
 export default useRouter;
